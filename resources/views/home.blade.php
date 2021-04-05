7cc82886bcd9360b60b9326e8dbfb2ffcef69547
npv34 <phanvanluan611996@gmail.com>
2021-04-05 14:19:34 +0700

    fix css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <meta name="author" content="" />
     <title>Dashboard - SB Admin</title>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
-    <link href="{{ asset('backend/css/styles.css') }}" rel="stylesheet" />
+    <link href="{{ secure_asset('backend/css/styles.css') }}" rel="stylesheet" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
